2d4ce8a3625389e4ee859044939ac149c8e091d2
i-saint <saint.skr@gmail.com>
2014-06-11 21:48:25 +0900

    forgot to up scripts...

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneSwitcher : MonoBehaviour {
+
+	int scene = 0;
+
+	void Start()
+	{
+		scene = 0;
+		RenderSettings.skybox.SetInt("_Scene", scene);
+	}
+
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(10,10, 100, 30), "next scene"))
+		{
+			++scene;
+			RenderSettings.skybox.SetInt("_Scene", scene);
+		}
+	}
+}
